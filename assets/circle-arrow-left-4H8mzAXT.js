45e3c5380547ba1603f5004bcb3aa2c8f7df0d80
Md Hasan Mia <mdhasan972@gmail.com>
2025-03-05 00:46:19 +0600

    remove book page

--- assets/circle-arrow-left-4H8mzAXT.js
+++ assets/circle-arrow-left-4H8mzAXT.js
@@ -1,4 +1,4 @@
-import{e}from"./index-iRvyXJF3.js";/**
+import{e}from"./index-CA1e6CKk.js";/**
  * @license lucide-react v0.475.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/circle-arrow-up-BBoPViwp.js
+++ assets/circle-arrow-up-BBoPViwp.js
@@ -1,4 +1,4 @@
-import{e as c}from"./index-iRvyXJF3.js";/**
+import{e as c}from"./index-CA1e6CKk.js";/**
  * @license lucide-react v0.475.0 - ISC
  *
  * This source code is licensed under the ISC license.
